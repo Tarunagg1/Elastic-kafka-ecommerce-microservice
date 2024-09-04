6c7e938d345d6bb77d8b38571b9ff45a34c76e68
--add <tarun@iotric.com>
2024-09-04 21:19:45 +0530

    db oprations for catalog

--- catalog_service/src/repository/catalog.reposoitory.ts
+++ catalog_service/src/repository/catalog.reposoitory.ts
@@ -1,21 +1,54 @@
+import { NotFoundError } from "@prisma/client/runtime/library";
 import { ICatalogRepository } from "../interface/catelogRepository.interface";
 import { Product } from "../models/product.model";
+import { PrismaClient } from '@prisma/client';
+
 
 export class CatalogRepository implements ICatalogRepository {
+    _prisma: PrismaClient;
+
+    constructor() {
+        this._prisma = new PrismaClient();
+    }
+
     async create(data: Product): Promise<Product> {
-        throw new Error("Method not implemented.");
+        return this._prisma.product.create({ data });
+        // throw new Error("Method not implemented.");
     }
+
     async update(data: Product): Promise<Product> {
-        throw new Error("Method not implemented.");
-    }
-    async delete(id: number) {
-        throw new Error("Method not implemented.");
-    }
-    async find(limit: number, offset: number): Promise<Product[]> {
-        throw new Error("Method not implemented.");
-    }
-    async findOne(id: number): Promise<Product> {
+        return this._prisma.product.update({
+            where: {
+                id: data.id
+            }, data
+        });
         throw new Error("Method not implemented.");
     }
 
+    async delete(id: number) {
+        return this._prisma.product.delete({
+            where: { id },
+        });
+        throw new Error("Method not implemented.");
+    }
+
+    async find(limit: number, offset: number): Promise<Product[]> {
+        return this._prisma.product.findMany({
+            take: limit,
+            skip: offset,
+        });
+        throw new Error("Method not implemented.");
+    }
+
+    async findOne(id: number): Promise<Product> {
+        const product = await this._prisma.product.findFirst({
+            where: { id },
+        });
+        if (product) {
+            return Promise.resolve(product);
+        }
+        throw new Error("product not found.");
+
+        // throw new NotFoundError("product not found");
+    }
 }
